46593f3279cb820fd3165e5e538e27dc1b983807
Advait Maybhate <advait.maybhate9@gmail.com>
2016-10-22 15:41:57 -0400

    Adding map

---
+++ map.py
@@ -0,0 +1,16 @@
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+# llcrnrlat,llcrnrlon,urcrnrlat,urcrnrlon
+# are the lat/lon values of the lower left and upper right corners
+# of the map.
+# resolution = 'c' means use crude resolution coastlines.
+m = Basemap(projection='cyl',llcrnrlat=-90,urcrnrlat=90,\
+            llcrnrlon=-180,urcrnrlon=180,resolution='c')
+# draw parallels and meridians.
+m.drawparallels(np.arange(-90.,91.,30.))
+m.drawmeridians(np.arange(-180.,181.,60.))
+m.bluemarble()
+plt.title("Equidistant Cylindrical Projection")
+plt.show()
